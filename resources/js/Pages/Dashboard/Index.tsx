cd1f8bffe80daf01bb73c954e3c8b39307ecfb0a
HoangHaix86 <hai.hoang.2762@gmail.com>
2023-08-23 22:40:11 +0700

    update 1

---
+++ resources/js/Pages/Dashboard/Index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {ChakraProvider} from "@chakra-ui/react";
+
+const Index = () => {
+    return <ChakraProvider>
+        <h1>Dashboa</h1>
+    </ChakraProvider>
+}
+
+export default Index;
