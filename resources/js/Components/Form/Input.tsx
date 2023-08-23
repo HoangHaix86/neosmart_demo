07d5ddd28651c5042b776d772d3ca6cd44267264
HoangHaix86 <hai.hoang.2762@gmail.com>
2023-08-23 23:04:07 +0700

    update 2

--- resources/js/Components/Form/Input.tsx
+++ resources/js/Components/Form/Input.tsx
@@ -1,35 +1,37 @@
 import React from "react";
-import {Controller, useFormContext} from "react-hook-form";
-import {FormControl, FormErrorMessage, FormLabel} from "@chakra-ui/react";
+import { Controller, useFormContext } from "react-hook-form";
+import { FormControl, FormErrorMessage, FormLabel } from "@chakra-ui/react";
+import { Input as InputBase } from "@chakra-ui/react";
 
 interface IProps {
     name: string;
+    label: string;
+    type: string;
 }
 
-const Input: React.FC<IProps> = ({}) => {
-    const {control} = useFormContext()
+const Input: React.FC<IProps> = ({ name, label, type }) => {
+    const { control } = useFormContext()
 
-    return 1
-    // return <Controller
-    //     control={control}
-    //     name={name}
-    //     render={() => {
-    //         // <FormControl isInvalid={errors.name}>
-    //         //     <FormLabel htmlFor='name'>First name</FormLabel>
-    //         //     <Input
-    //         //         id='name'
-    //         //         placeholder='name'
-    //         //         {...register('name', {
-    //         //             required: 'This is required',
-    //         //             minLength: { value: 4, message: 'Minimum length should be 4' },
-    //         //         })}
-    //         //     />
-    //         //     <FormErrorMessage>
-    //         //         {errors.name && errors.name.message}
-    //         //     </FormErrorMessage>
-    //         // </FormControl>
-    //     }}
-    // />
+    return <Controller
+        control={control}
+        name={name}
+        render={({ field: { onChange, name, value }, fieldState: { error }, formState }) => {
+            return (
+                <FormControl>
+                    <FormLabel htmlFor={name}>{label}</FormLabel>
+                    <InputBase
+                        name={name}
+                        type={type}
+                        onChange={onChange}
+                        value={value}
+                    />
+                    <FormErrorMessage>
+                        {/* {errors.name && errors.name.message} */}
+                    </FormErrorMessage>
+                </FormControl >)
+
+        }}
+    />
 }
 
 export default Input;
--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -1,77 +1,70 @@
 import {
     Flex,
     Box,
-    FormControl,
-    FormLabel,
-    Input,
     Checkbox,
     Stack,
     Button,
     Heading,
     Text,
     useColorModeValue,
-    ChakraProvider,
 } from '@chakra-ui/react'
-import React, {useState} from 'react'
-import {usePage} from "@inertiajs/inertia-react";
-
-interface IFormState {
-    [key: string]: string;
-}
+import React, { useState } from 'react'
+import { usePage } from "@inertiajs/inertia-react";
+import { FormProvider, useForm } from 'react-hook-form';
+import Input from '../../Components/Form/Input';
 
 export default function Login() {
-    const {errors} = usePage().props
-    const [form, setForm] = useState<IFormState>()
+    const { errors } = usePage().props
+    const methods = useForm<any>();
+    const onSubmit = (value: any) => {
+        console.log(value)
+    }
 
     return (
-        <ChakraProvider>
-            <Flex
-                minH={'100vh'}
-                align={'center'}
-                justify={'center'}
-                bg={useColorModeValue('gray.50', 'gray.800')}>
-                <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-                    <Stack align={'center'}>
-                        <Heading fontSize={'4xl'}>Sign in to your account</Heading>
-                        <Text fontSize={'lg'} color={'gray.600'}>
-                            to enjoy all of our cool <Text color={'blue.400'}>features</Text> ✌️
-                        </Text>
-                    </Stack>
-                    <Box
-                        rounded={'lg'}
-                        bg={useColorModeValue('white', 'gray.700')}
-                        boxShadow={'lg'}
-                        p={8}>
-                        <Stack spacing={4}>
-                            <FormControl id="email">
-                                <FormLabel>Username or Email</FormLabel>
-                                <Input type="text"/>
-                            </FormControl>
-                            <FormControl id="password">
-                                <FormLabel>Password</FormLabel>
-                                <Input type="password"/>
-                            </FormControl>
-                            <Stack spacing={10}>
-                                <Stack
-                                    direction={{base: 'column', sm: 'row'}}
-                                    align={'start'}
-                                    justify={'space-between'}>
-                                    <Checkbox>Remember me</Checkbox>
-                                    <Text color={'blue.400'}>Forgot password?</Text>
-                                </Stack>
-                                <Button
-                                    bg={'blue.400'}
-                                    color={'white'}
-                                    _hover={{
-                                        bg: 'blue.500',
-                                    }}>
-                                    Sign in
-                                </Button>
-                            </Stack>
-                        </Stack>
-                    </Box>
+        <Flex
+            minH={'100vh'}
+            align={'center'}
+            justify={'center'}
+            bg={useColorModeValue('gray.50', 'gray.800')}>
+            <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+                <Stack align={'center'}>
+                    <Heading fontSize={'4xl'}>Sign in to your account</Heading>
+                    <Text fontSize={'lg'} color={'gray.600'}>
+                        to enjoy all of our cool <Text color={'blue.400'}>features</Text> ✌️
+                    </Text>
                 </Stack>
-            </Flex>
-        </ChakraProvider>
+                <Box
+                    rounded={'lg'}
+                    bg={useColorModeValue('white', 'gray.700')}
+                    boxShadow={'lg'}
+                    p={8}>
+                    <FormProvider {...methods}>
+                        <form onSubmit={methods.handleSubmit(onSubmit)}>
+                            <Stack spacing={4}>
+                                <Input name='username' type='text' label='Username' />
+                                <Input name='password' type='password' label='Password' />
+                                <Stack spacing={10}>
+                                    <Stack
+                                        direction={{ base: 'column', sm: 'row' }}
+                                        align={'start'}
+                                        justify={'space-between'}>
+                                        <Checkbox>Remember me</Checkbox>
+                                        <Text color={'blue.400'}>Forgot password?</Text>
+                                    </Stack>
+                                    <Button
+                                        bg={'blue.400'}
+                                        color={'white'}
+                                        _hover={{
+                                            bg: 'blue.500',
+                                        }}>
+                                        Sign in
+                                    </Button>
+                                </Stack>
+                            </Stack>
+                        </form>
+                    </FormProvider>
+                </Box>
+            </Stack>
+        </Flex>
     )
 }
