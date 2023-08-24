1d8fdbe707b78e83dbec59625884ac1f500b3909
HoangHaix86 <hai.hoang.2762@gmail.com>
2023-08-24 11:28:23 +0700

    create InputFIle

---
+++ resources/js/Components/Form/InputFile.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {Controller, useFormContext} from "react-hook-form";
+import {FormControl, FormErrorMessage, FormLabel} from "@chakra-ui/react";
+import {Input as InputBase} from "@chakra-ui/react";
+
+interface IProps {
+    name: string;
+    label: string;
+    type: string;
+}
+
+const Input: React.FC<IProps> = ({name, label, type}) => {
+    const {control} = useFormContext()
+
+    return <Controller
+        control={control}
+        name={name}
+        render={({field: {onChange, name, value}, fieldState: {error}}) => {
+            return (
+                <FormControl isInvalid={!!error}>
+                    <FormLabel htmlFor={name}>{label}</FormLabel>
+                    <InputBase
+                        name={name}
+                        type={type}
+                        onChange={onChange}
+                        value={value}
+                    />
+                    <FormErrorMessage>
+                        {error?.message}
+                    </FormErrorMessage>
+                </FormControl>)
+
+        }}
+    />
+}
+
+export default Input;
