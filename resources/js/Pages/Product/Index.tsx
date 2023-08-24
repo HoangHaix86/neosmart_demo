760038c14b96a739148b27142b30acfa13fc08a2
Your Name <84726273+hieuhunter@users.noreply.github.com>
2023-08-24 16:59:27 +0700

    update data table

--- resources/js/Pages/Product/Index.tsx
+++ resources/js/Pages/Product/Index.tsx
@@ -2,13 +2,66 @@ import React from "react";
 import Table from "@/Components/Table/Table";
 import { Flex } from "@chakra-ui/react";
 
+const dataProduct = [
+    {
+        id: 1,
+        name: "tets",
+        price: "10000",
+        create_at: "2021-01-01",
+        update_at: "2021-01-01",
+    },
+    {
+        id: 1,
+        name: "tets1",
+        price: "10000",
+        create_at: "2021-01-01",
+        update_at: "2021-01-01",
+    },
+    {
+        id: 1,
+        name: "tets2",
+        price: "10000",
+        create_at: "2021-01-01",
+        update_at: "2021-01-01",
+    },
+    {
+        id: 1,
+        name: "tets3",
+        price: "10000",
+        create_at: "2021-01-01",
+        update_at: "2021-01-01",
+    },
+    {
+        id: 1,
+        name: "tets4",
+        price: "10000",
+        create_at: "2021-01-01",
+        update_at: "2021-01-01",
+    },
+    {
+        id: 1,
+        name: "tets5",
+        price: "10000",
+        create_at: "2021-01-01",
+        update_at: "2021-01-01",
+    },
+    {
+        id: 1,
+        name: "tets6",
+        price: "10000",
+        create_at: "2021-01-01",
+        update_at: "2021-01-01",
+    },
+];
+
+const colums = ["STT", "Tên", "Giá", "Ngày tạo", "Ngày cập nhật"];
 const Index = () => {
     return (
         <Flex direction="column" pt={{ base: "120px", md: "75px" }}>
             <Table
                 title={"Authors Table"}
-                captions={["Author", "Function", "Status", "Employed", ""]}
-                data={[]}
+                captions={colums}
+                data={dataProduct || []}
             />
         </Flex>
     );
