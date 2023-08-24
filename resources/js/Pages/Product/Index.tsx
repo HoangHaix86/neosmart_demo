fad59880d2ea3c036a90fe3556c4fb6409ed817c
Your Name <84726273+hieuhunter@users.noreply.github.com>
2023-08-24 15:23:46 +0700

    update code

--- resources/js/Pages/Product/Index.tsx
+++ resources/js/Pages/Product/Index.tsx
@@ -1,5 +1,16 @@
+import Table from "@/Components/Table/Table";
+import { Flex } from "@chakra-ui/react";
+
 const Index = () => {
-    return (<h1>Product</h1>);
-}
+    return (
+        <Flex direction="column" pt={{ base: "120px", md: "75px" }}>
+            <Table
+                title={"Authors Table"}
+                captions={["Author", "Function", "Status", "Employed", ""]}
+                data={[]}
+            />
+        </Flex>
+    );
+};
 
 export default Index;
