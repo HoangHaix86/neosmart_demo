57390c1f8f5cbf0bbf23ef59db775153c0e7627e
HoangHaix86 <hai.hoang.2762@gmail.com>
2023-08-24 16:16:39 +0700

    template table

--- resources/js/Pages/Contact/Index.tsx
+++ resources/js/Pages/Contact/Index.tsx
@@ -1,8 +1,33 @@
+import {Box, TableContainer, Tbody, Td, Table, Th, Thead, Tr} from "@chakra-ui/react";
+
 const Index = () => {
     return (
-        <div>
-            <h1>Contact</h1>
-        </div>
+        <Box __css={{
+            bg: 'white',
+            borderRadius: 'md',
+        }}>
+            <TableContainer>
+                <Table variant='simple'>
+                    <Thead>
+                        <Tr>
+                            <Th>Name</Th>
+                            <Th>Email</Th>
+                            <Th>Phone</Th>
+                            <Th>Content</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        <Tr>
+                            <Td>inches</Td>
+                            <Td>millimetres (mm)</Td>
+                            <Td>25.4</Td>
+                            <Td>25.4</Td>
+                        </Tr>
+
+                    </Tbody>
+                </Table>
+            </TableContainer>
+        </Box>
     );
 }
 export default Index;
